6f1ac26d1d7287edbcb9132dc66758c89c8753b6
Nasser Alkmim <nasser.alkmim@gmail.com>
2021-07-17 08:54:59 +0200

    Changed Labels class to a dataclass.

--- portman/labels.py
+++ portman/labels.py
@@ -1,25 +1,25 @@
 """Define string labels for DataFrames columns and for plotting."""
+from dataclasses import dataclass
 
-
+@dataclass
 class Labels:
     """Define labels."""
-    def __init__(self):
-        self.AVG_PRICE = "average price ($)"  # average purchase price of an asset
-        self.PL = "P/L %"  # profit and loss in a position
-        self.MARKET_PRICE = "market price ($)"  # current price
-        self.MARKET_VALUE = "market value ($)"  # current value of a position
-        self.SECTOR = "sector"  # asset business sector
-        self.DATE = "date"
-        self.TYPE = "type"  # buy, sell, split from the trade data
-        self.TICKER = "ticker"
-        self.SHARES = "shares"
-        self.ADJUSTED_VOL = "volume adjusted"  # adjusted volume based on type
-        self.PURCHASE_PRICE = "purchase price"
-        self.TOTAL = "total"
-        self.FEE = "fee"
-        self.BUY = "Buy"
-        self.SELL = "Sell"
-        self.SPLIT = "Split"
+    AVG_PRICE: str = "average price ($)"  # average purchase price of an asset
+    PL: str = "P/L %"  # profit and loss in a position
+    MARKET_PRICE: str = "market price ($)"  # current price
+    MARKET_VALUE: str = "market value ($)"  # current value of a position
+    SECTOR: str = "sector"  # asset business sector
+    DATE: str = "date"
+    TYPE: str = "type"  # buy, sell, split from the trade data
+    TICKER: str = "ticker"
+    SHARES: str = "shares"
+    ADJUSTED_VOL: str = "volume adjusted"  # adjusted volume based on type
+    PURCHASE_PRICE: str = "purchase price"
+    TOTAL: str = "total"
+    FEE: str = "fee"
+    BUY: str = "Buy"
+    SELL: str = "Sell"
+    SPLIT: str = "Split"
 
     def set_label(self, **kwarg: str) -> None:
         """Change or add new label."""
