43775504132c4dfd1336363a8c33ffb887d5cece
Nasser Alkmim <nasser.alkmim@gmail.com>
2021-07-17 18:26:19 +0200

    Modified Portfolio methods returns to not change the dataframe.

--- portman/portfolio.py
+++ portman/portfolio.py
@@ -6,7 +6,7 @@ from portman.trades import Trades
 
 
 class Portfolio:
-    """Creates portfolio object"""
+    """Creates portfolio object."""
 
     def __init__(self, trades: Trades, portfolio_file: str = "portfolio.csv"):
         self.labels = trades.labels  # aggregation of Labels object
@@ -16,42 +16,42 @@ class Portfolio:
         self.trades.history = self.trades.adjusted_volume()
 
         self.portfolio = pd.DataFrame()
-        self.portfolio = self._net_position()
-        self.portfolio = self._average_purchase_price()
-        self.portfolio = self._current_price()
-        self.portfolio = self._profit_and_loss()
-        self.portfolio = self._market_value()
-        self.portfolio = self._sector()
+        self.portfolio[self.labels.SHARES] = self._net_shares()
+        self.portfolio[self.labels.AVG_PRICE] = self._average_purchase_price()
+        # remove net 0 positions
+        self.portfolio = self.portfolio[self.portfolio[self.labels.SHARES] != 0]
+        self.portfolio[self.labels.MARKET_PRICE] = self._current_price()
+        self.portfolio[self.labels.PL] = self._profit_and_loss()
+        self.portfolio[self.labels.MARKET_VALUE] = self._market_value()
+        self.portfolio[self.labels.SECTOR] = self._sector()
         self.portfolio.to_csv(portfolio_file)
 
-    def _net_position(self) -> pd.DataFrame:
-        """Computes net position from trades and adds column."""
-        self.portfolio[self.labels.SHARES] = self.trades.history.groupby(
+    def _net_shares(self) -> pd.DataFrame:
+        """Computes net position from trades."""
+        net_shares = self.trades.history.groupby(
             self.labels.TICKER
         )[self.labels.ADJUSTED_VOL].sum()
-        return self.portfolio
+        return net_shares
 
     def _average_purchase_price(self) -> pd.DataFrame:
-        """Computes average purchase price of assets and adds column."""
+        """Computes average purchase price of assets."""
         # naive, simple, implementation, just sum total and divide by net position
-        self.portfolio[self.labels.AVG_PRICE] = (
+        avg_price = (
             self.trades.history.groupby("ticker")[self.labels.TOTAL].sum()
             / self.portfolio[self.labels.SHARES]
         )
-        # remove net 0 positions
-        self.portfolio = self.portfolio[self.portfolio[self.labels.SHARES] != 0]
-        return self.portfolio
+        return avg_price
 
     def _current_price(self) -> pd.DataFrame:
-        """Get current price from Yahoo finance and adds column."""
-        self.portfolio[self.labels.MARKET_PRICE] = self.portfolio.apply(
+        """Get current price from Yahoo finance."""
+        market_price = self.portfolio.apply(
             lambda x: yf.Ticker(x.name).quotes[x.name]["regularMarketPrice"], axis=1
         )
-        return self.portfolio
+        return market_price
 
     def _profit_and_loss(self) -> pd.DataFrame:
-        """Computes profit and loss and adds column."""
-        self.portfolio[self.labels.PL] = (
+        """Computes profit and loss."""
+        profit_loss = (
             (
                 self.portfolio[self.labels.MARKET_PRICE]
                 - self.portfolio[self.labels.AVG_PRICE]
@@ -59,20 +59,20 @@ class Portfolio:
             / self.portfolio[self.labels.AVG_PRICE]
             * 100
         )
-        return self.portfolio
+        return profit_loss 
 
     def _market_value(self) -> pd.DataFrame:
-        """Compute current value and add column to summary."""
-        self.portfolio[self.labels.MARKET_VALUE] = (
+        """Compute current value from number of shares and market price."""
+        market_value = (
             self.portfolio[self.labels.MARKET_PRICE]
             * self.portfolio[self.labels.SHARES]
         )
-        return self.portfolio
+        return market_value
 
     def _sector(self) -> pd.DataFrame:
-        """Get assets business sector and adds column."""
-        self.portfolio[self.labels.SECTOR] = self.portfolio.apply(
+        """Get assets business sector."""
+        sector = self.portfolio.apply(
             lambda x: yf.Ticker(x.name).asset_profile[x.name]["sector"],
             axis=1,
         )
-        return self.portfolio
+        return sector
