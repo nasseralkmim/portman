d8319ae68431a306aacbc07261b0f5e4d226938b
Nasser Alkmim <nasser.alkmim@gmail.com>
2021-07-22 06:52:34 +0200

    Renamed examples subfolder.

--- examples/two_sources/consolidation_example.py
+++ examples/two_sources/consolidation_example.py
@@ -7,9 +7,8 @@ pd.set_option('display.max_columns', None)
 
 stocks_trades = portman.trades.Trades('stocks.csv')
 etf_trades = portman.trades.Trades('etf.csv')
-etfs = portman.portfolio.Portfolio([etf_trades,
-                                   stocks_trades])
-
+etfs = portman.portfolio.Portfolio(etf_trades)
+stocks = portman.portfolio.Portfolio(stocks_trades) 
 
 print(etfs.portfolio)
 print(stocks.portfolio)
